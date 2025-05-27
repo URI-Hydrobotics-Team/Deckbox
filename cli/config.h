b5538114ce90cfaf78fb253c5e314bf786920c3f
Christopher McRae <roach@crackbook>
2025-05-27 18:54:54 -0400

    initial socket implementation

--- cli/config.h
+++ cli/config.h
@@ -1,2 +1,7 @@
 std::string version_string = "alpha 0";
 
+//port and ip definitions
+
+#define AUVHUBIP "127.0.0.1"
+#define AUVHUBPORT 8100
+
--- cli/main.cpp
+++ cli/main.cpp
@@ -13,14 +13,25 @@
 #include "config.h"
 
 
+/* AUV HUB */
+char *auv_hub_ip = AUVHUBIP;
+int auv_hub_port = AUVHUBPORT;
+
+
+/*socket definitions*/
+char rx_buffer[256];
+void socketInit(char *host, int port);
+void socketReceive();
+
+
 
 void printHelp(){
 	std::cout << "DeckBox-CLI version: " << version_string << "\n";
 	std::cout << "\nUSAGE:\n";
 	std::cout << "\t\tdeckbox-cli <mode> <arguments>\n"; 
-	
+		
 	std::cout << "\tModes: help, listen\n";
-	std::cout << "\tListen Arguments: <ip>\n";	
+	std::cout << "\tListen Arguments: \n";	
 
 
 
@@ -31,12 +42,69 @@ void printHelp(){
 void listen(){
 	std::cout << "DeckBox-CLI version: " << version_string << "\n";
 		
-	
+	//socketInit(auv_hub_ip, auv_hub_port); // display messages from auv hub	
 
-
-    	printf("\033[H\033[J"); //clear screen
+	socketInit("127.0.0.1", 8100); // display messages from auv hub	
+	//socketReceive();
+    	//printf("\033[H\033[J"); //clear screen
 	listen();
 }
+
+
+
+void socketInit(char *host, int port){
+
+	
+	struct sockaddr_in my_addr;	/* our address */
+	struct sockaddr_in remote_addr;	/* remote address */
+	socklen_t addrlen = sizeof(remote_addr);		/* length of addresses */
+	int recvlen;			/* # bytes received */
+	int fd;				/* the socket */
+
+
+
+
+	/*
+	if (fd = socket(AF_INET, SOCK_DGRAM, 0) < 0){
+		//create UDP socket and verify if succesful
+		std::cout << "socket creation failure\n";	
+
+
+	}
+	*/
+	fd = socket(AF_INET, SOCK_DGRAM, 0);
+
+	memset((char *)&my_addr, 0, sizeof(my_addr));
+	my_addr.sin_family = AF_INET;
+	//my_addr.sin_addr.s_addr = htonl(std::stoi(host));
+	//TODO allow user to select ip
+	my_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+	my_addr.sin_port = htons(port);
+	if (bind(fd, (struct sockaddr *)&my_addr, sizeof(my_addr)) < 0) {
+		//std::cout << "bind to " << host ":" << port << "failed\n";
+		std::cout << "failed to bind socket\n";
+	}
+
+	std::cout << "working\n";
+//	while(1){
+	recvlen = recvfrom(fd,rx_buffer, 256, 0, (struct sockaddr *)&remote_addr, &addrlen);
+		if (recvlen > 0) {
+			printf("received %d bytes\n", recvlen);
+			rx_buffer[recvlen] = 0;
+			printf("received message: \"%s\"\n",rx_buffer);
+		}
+//	}
+
+
+	shutdown(fd, 2);
+
+}
+
+
+
+
+
+
 
 
 
@@ -61,6 +129,8 @@ int main(int argc, char *argv[]){
 	//display help
 	if (mode == "listen"){
 		listen();
+		
+
 	}
 
 

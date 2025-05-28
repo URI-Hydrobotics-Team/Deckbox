b46a4025b5d5e793094a4be17272db3dc125e8df
Christopher McRae <roach@crackbook>
2025-05-28 02:00:36 -0400

    more refined socket implementation

--- cli/README.md
+++ cli/README.md
@@ -3,6 +3,7 @@
 A console based DeckBox interface for debugging and testing only. See the PyQT interface for the production version.
 
 # Overview
+The program checks sockets defined in the config.h file. Currently there is no process for polling these sockets.
 
 # Compilation
 
--- cli/main.cpp
+++ cli/main.cpp
@@ -9,7 +9,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h> 
-
+#include <fcntl.h> //include fentanyl
 #include "config.h"
 
 
@@ -20,8 +20,21 @@ int auv_hub_port = AUVHUBPORT;
 
 /*socket definitions*/
 char rx_buffer[256];
-void socketInit(char *host, int port);
-void socketReceive();
+void hubSocketInit(char *host, int port);
+
+/*function declerations */
+void hubSocketRec();
+
+/*
+	Socket device setup 
+	This is currently for the hub connection, should be put in a "connections" header file
+
+*/
+	struct sockaddr_in my_addr;	/* our address */
+	struct sockaddr_in remote_addr;	/* remote address */
+	socklen_t addrlen = sizeof(remote_addr);		/* length of addresses */
+	int recvlen;			/* # bytes received */
+	int fd;				/* the socket */
 
 
 
@@ -39,40 +52,58 @@ void printHelp(){
 }
 
 
-void listen(){
+
+void listen(){ 
+	/* 
+		Main function for defining which sockets to listen to
+	*/
 	std::cout << "DeckBox-CLI version: " << version_string << "\n";
 		
 	//socketInit(auv_hub_ip, auv_hub_port); // display messages from auv hub	
 
-	socketInit("127.0.0.1", 8100); // display messages from auv hub	
-	//socketReceive();
+	hubSocketInit("127.0.0.1", 8100); // display messages from auv hub	
+	hubSocketRec();
+	//do other things
+
     	//printf("\033[H\033[J"); //clear screen
-	listen();
+	
+
+
 }
 
 
 
-void socketInit(char *host, int port){
+void hubSocketRec(){
 
+	/* Function for recieving from AUV HUB*/
 	
-	struct sockaddr_in my_addr;	/* our address */
-	struct sockaddr_in remote_addr;	/* remote address */
-	socklen_t addrlen = sizeof(remote_addr);		/* length of addresses */
-	int recvlen;			/* # bytes received */
-	int fd;				/* the socket */
-
-
-
-
-	/*
-	if (fd = socket(AF_INET, SOCK_DGRAM, 0) < 0){
-		//create UDP socket and verify if succesful
-		std::cout << "socket creation failure\n";	
-
-
+	recvlen = recvfrom(fd,rx_buffer, 256, 0, (struct sockaddr *)&remote_addr, &addrlen);
+	if (recvlen > 0) {
+		/* 
+		define how the message is handled 
+		we would like to have a "message box" in the interface where raw message data is put
+		*/
+		printf("received %d bytes\n", recvlen);
+		rx_buffer[recvlen] = 0;
+		printf("received message: \"%s\"\n",rx_buffer);
 	}
-	*/
+
+	shutdown(fd, 2);
+	usleep(100000); //sleep for 100ms
+
+	hubSocketRec(); // recursive call for testing
+}
+
+
+
+void hubSocketInit(char *host, int port){
+	//TODO move to another file
+	/* Initialize a socket */
 	fd = socket(AF_INET, SOCK_DGRAM, 0);
+
+	//set fd to non blocking
+	int flags = fcntl(fd, F_GETFL, 0);
+	fcntl(fd, F_SETFL, flags | O_NONBLOCK);
 
 	memset((char *)&my_addr, 0, sizeof(my_addr));
 	my_addr.sin_family = AF_INET;
@@ -85,19 +116,7 @@ void socketInit(char *host, int port){
 		std::cout << "failed to bind socket\n";
 	}
 
-	std::cout << "working\n";
-//	while(1){
-	recvlen = recvfrom(fd,rx_buffer, 256, 0, (struct sockaddr *)&remote_addr, &addrlen);
-		if (recvlen > 0) {
-			printf("received %d bytes\n", recvlen);
-			rx_buffer[recvlen] = 0;
-			printf("received message: \"%s\"\n",rx_buffer);
-		}
-//	}
-
-
-	shutdown(fd, 2);
-
+	std::cout << "HUB Socket Initialized\n";
 }
 
 
